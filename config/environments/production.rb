0edbccdeb695f20f9982af4ce6754be7a96f9fd4
ishida_masakazu <u225210b@gmail.com>
2018-08-03 20:43:58 +0900

    config/environments/production.rbのActionMailer設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://cryptic-eyrie-65109.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://morning-falls-96011.herokuapp.com/posts/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
